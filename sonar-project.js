82acc6975f1fe361bec38efe5644f8ecd519433d
Mangarapu Raghu <125724523+omairaghu@users.noreply.github.com>
2023-11-03 23:22:58 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.232.170.87:9000/',
+  serverUrl: 'http://172.31.37.94:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
